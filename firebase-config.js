b41a782422dff946e8f31e573d99dff081d8245d
Paul Adamu <pauladamu600@gmail.com>
2026-05-13 13:03:27 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyAUVg8cgabMYHuiJzykEwvln8WtJA1KwTQ",
+  authDomain: "montra-app-317c1.firebaseapp.com",
+  projectId: "montra-app-317c1",
+  storageBucket: "montra-app-317c1.firebasestorage.app",
+  messagingSenderId: "659772633871",
+  appId: "1:659772633871:web:795b58ee7599d62173447a"
+};
