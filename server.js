ef2b989a73455fd100269d04dbed64529e8604b0
Paul Adamu <pauladamu600@gmail.com>
2026-05-13 12:58:14 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ const MIME = {
 
 // Firebase client config injected from server env vars
 const firebaseConfig = {
-  apiKey:            process.env.FIREBASE_API_KEY            || '',
-  authDomain:        process.env.FIREBASE_AUTH_DOMAIN        || '',
-  projectId:         process.env.FIREBASE_PROJECT_ID         || '',
-  storageBucket:     process.env.FIREBASE_STORAGE_BUCKET     || '',
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID|| '',
-  appId:             process.env.FIREBASE_APP_ID             || '',
+  apiKey:            process.env.FIREBASE_API_KEY            || process.env.VITE_FIREBASE_API_KEY            || '',
+  authDomain:        process.env.FIREBASE_AUTH_DOMAIN        || process.env.VITE_FIREBASE_AUTH_DOMAIN        || '',
+  projectId:         process.env.FIREBASE_PROJECT_ID         || process.env.VITE_FIREBASE_PROJECT_ID         || '',
+  storageBucket:     process.env.FIREBASE_STORAGE_BUCKET     || process.env.VITE_FIREBASE_STORAGE_BUCKET     || '',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID|| process.env.VITE_FIREBASE_MESSAGING_SENDER_ID|| '',
+  appId:             process.env.FIREBASE_APP_ID             || process.env.VITE_FIREBASE_APP_ID             || '',
 };
 
 const firebaseConfigJS = `window.__FIREBASE_CONFIG__ = ${JSON.stringify(firebaseConfig)};`;
